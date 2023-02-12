f66007f8211bfa6bb10435adadc24a7b732025e8
wojtko <j.w2001@onet.pl>
2023-02-12 01:06:27 +0100

    Add resizing function and improve controls

--- RotatingMoon/main.js
+++ RotatingMoon/main.js
@@ -1,15 +1,21 @@
 import * as THREE from './node_modules/three/build/three.module.js';
-//import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
 
 // scene, camera, render
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 45, sizes.width / sizes.height, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio( window.devicePixelRatio );
 camera.position.setZ(30);
 
@@ -30,26 +36,37 @@ scene.add( sphere );
 // lights
 const pointLight = new THREE.PointLight({ color: 0xFFFFFF});
 const ambientLight = new THREE.AmbientLight({ color: 0xFFFFFF });
-pointLight.position.set(20, 20, 20);
+pointLight.position.set(0, 20, 20);
 
 scene.add( pointLight, ambientLight );
 
-/* helpers
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add( lightHelper, gridHelper );
-*/
+// resize functionality
+window.addEventListener('resize', () => {
+    // update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-// const controls = new OrbitControls(camera, renderer.domElement);
+    //update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+});
+
+const canvas = document.querySelector('canvas');
+
+//controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true; // to not stop instantly
+controls.enablePan = false; 
 
 function animate() {  // game loop, to not update our render over and over again when we add a new obj to the scene
-    requestAnimationFrame( animate );
+    window.requestAnimationFrame( animate );
 
     sphere.rotation.x += 0.01;
     sphere.rotation.y += 0.005;
     sphere.rotation.z += 0.01;
 
-    // controls.update();
+    controls.update();
     
     renderer.render( scene, camera );
 }
