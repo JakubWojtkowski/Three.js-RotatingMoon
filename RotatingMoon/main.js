2000096fd0d61d8f3bc071423987061def4cf28e
wojtko <j.w2001@onet.pl>
2023-02-12 01:50:58 +0100

    Add textures correctly and improve the lights.

--- RotatingMoon/main.js
+++ RotatingMoon/main.js
@@ -18,27 +18,39 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio( window.devicePixelRatio );
 camera.position.setZ(30);
+camera.lookAt( 0, 0, 0 );
 
 renderer.render( scene, camera );
 
 // geometry, material and mesh
 const geometry = new THREE.SphereGeometry(3, 64, 16);
 const material = new THREE.MeshStandardMaterial( {
-     color: 0xFF6347,
+     color: 0xFFFFFF,
+     metalness: 0.1,
+     roughness: 0.9
      } );
 const sphere = new THREE.Mesh(geometry, material);
 
 // texture of moon
-const texture = new THREE.TextureLoader('moon.jpg');
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+material.map = moonTexture;
 
 scene.add( sphere );
 
 // lights
-const pointLight = new THREE.PointLight({ color: 0xFFFFFF});
-const ambientLight = new THREE.AmbientLight({ color: 0xFFFFFF });
-pointLight.position.set(0, 20, 20);
+const ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.01);
+scene.add( ambientLight );
 
-scene.add( pointLight, ambientLight );
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(-30, 2, 10);
+light.target.position.set(0, 0, 0);
+// light.castShadow = true;
+scene.add(light);
+
+// texture of moon - bump texture
+const moonBump = new THREE.TextureLoader().load('normal.jpg');
+material.bumpMap = moonTexture;
+material.bumpScale = 0.2;
 
 // resize functionality
 window.addEventListener('resize', () => {
@@ -57,14 +69,18 @@ const canvas = document.querySelector('canvas');
 //controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true; // to not stop instantly
-controls.enablePan = false; 
+controls.enablePan = false;  
+controls.enableZoom = false; // to disable zoom-in and stuff like that
+controls.autoRotate = true; // to auto rotate
+controls.autoRotateSpeed = 3;
 
-function animate() {  // game loop, to not update our render over and over again when we add a new obj to the scene
+// loop to not update our render over and over again when we add a new obj to the scene
+function animate() {
     window.requestAnimationFrame( animate );
 
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.005;
-    sphere.rotation.z += 0.01;
+    //sphere.rotation.x += 0.01;
+    // sphere.rotation.y += 0.005;
+    // sphere.rotation.z += 0.01;
 
     controls.update();
     
